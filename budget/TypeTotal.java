8220b8512943072befaa7c3b72d8b6af46b175c0
Valeri <prohovaleri@gmail.com>
2026-01-13 18:42:55 +0100

    Add files via upload

---
+++ budget/TypeTotal.java
@@ -0,0 +1,11 @@
+package budget;
+
+public class TypeTotal {
+    public final String category;
+    public final double sum;
+
+    public TypeTotal(String category, double sum) {
+        this.category = category;
+        this.sum = sum;
+    }
+}
